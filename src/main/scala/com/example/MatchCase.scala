8c4186d9eb726cfab86ff104c76439f5c652170a
Anatol <seddboom@gmail.com>
2023-02-01 09:23:52 +0300

    add MatchCase.scala

---
+++ src/main/scala/com/example/MatchCase.scala
@@ -0,0 +1,17 @@
+package com.example
+
+object MatchCase {
+  def main(args: Array[String]) {
+    println(specialtyTest("Java"));
+    println(specialtyTest("C++"));
+    println(specialtyTest("Scala"));
+    println(specialtyTest("PHP"));
+  }
+
+  def specialtyTest(specialty: String): String = specialty match {
+    case "Scala" => "Scala Developer"
+    case "Java" => "Java Developer"
+    case "C++" => "C++ Developer"
+    case _ => "Unsupported specialty"
+  }
+}
