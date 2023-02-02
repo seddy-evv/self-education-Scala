21864697dd1eaf2877107f179581882802fdae78
Anatol <seddboom@gmail.com>
2023-02-02 09:40:07 +0300

    add Exceptions.scala

---
+++ src/main/scala/com/example/Exceptions.scala
@@ -0,0 +1,25 @@
+package com.example
+
+object Exceptions {
+  def main(args: Array[String]) {
+    try {
+      val result = 100 / 0;
+      println(result);
+    } catch {
+      // Curly braces can be removed
+      case ex: ArithmeticException => {
+        println(ex);
+      }
+    }
+    // Try-exception with finally
+    try {
+      val result = 100 / 0;
+      println(result);
+    } catch {
+      case ex: ArithmeticException =>
+        println("ArithmeticException - cannot divide by zero");
+    }finally {
+      println("Block finally. This message will be shown in most cases.");
+    }
+  }
+}
