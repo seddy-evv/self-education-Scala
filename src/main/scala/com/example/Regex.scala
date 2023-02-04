a1a5537da0cfce939e707052833abf45d47b9831
Anatol <seddboom@gmail.com>
2023-02-04 13:07:05 +0300

    add Regex.scala

---
+++ src/main/scala/com/example/Regex.scala
@@ -0,0 +1,16 @@
+package com.example
+
+
+object Regex {
+  def main(args: Array[String]) {
+    val pattern = "Scala Developer".r;
+    val scalaDeveloper = "Eugene Suleimanov is a Java/Scala Developer";
+
+    println(pattern findFirstIn scalaDeveloper);
+
+    val patternC = "C".r;
+    val scalaDeveloperAlex = "Alex is C developer";
+
+    println(patternC replaceAllIn(scalaDeveloperAlex, "Java/Scala"));
+  }
+}
