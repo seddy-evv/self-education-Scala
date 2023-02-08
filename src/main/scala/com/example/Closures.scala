69ccb31b1251bd5d7ad5370f337bc3a4c74dc9c4
Anatol <seddboom@gmail.com>
2023-02-08 09:16:08 +0300

    add Closures.scala

---
+++ src/main/scala/com/example/Closures.scala
@@ -0,0 +1,12 @@
+package com.example
+
+object Closures {
+  def main(args: Array[String]) {
+    println("devider(300): " + devider(300));
+    println("devider(600): " + devider3(600));
+  }
+
+  var deviderFactor = 2;
+  val devider = (i: Int) => i / deviderFactor;
+  val devider3 = (i: Int) => i / 3;
+}
