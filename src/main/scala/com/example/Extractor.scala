d237369abaf4ae752c6bf595f6aa17b05612a3f8
Anatol <seddboom@gmail.com>
2023-02-10 09:15:22 +0300

    add Extractor.scala

---
+++ src/main/scala/com/example/Extractor.scala
@@ -0,0 +1,25 @@
+package com.example
+
+// An extractor is an object that has a method to remove one of its members.
+
+object Extractor {
+  def main(args: Array[String]) {
+    println(apply("Eugene Suleimanov", "Java/Scala Developer"));
+    println(unapply("Ivan Ivanov: C++ Developer"));
+    println(unapply("Kolya Nikolaev - PHP Developer"));
+  }
+
+  def apply(name: String, specialty: String): String = {
+    name + ": " + specialty
+  }
+
+  def unapply(str: String): Option[(String, String)] = {
+    val stringParts = str split ":"
+
+    if (stringParts.length == 2) {
+      Some(stringParts(0), stringParts(1))
+    } else {
+      None
+    }
+  }
+}
