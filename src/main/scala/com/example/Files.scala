46d9bed6a60969a0ea0c4092a8686da9efde8c29
Anatol <seddboom@gmail.com>
2023-02-13 09:21:14 +0300

    Add Files.scala

---
+++ src/main/scala/com/example/Files.scala
@@ -0,0 +1,19 @@
+package com.example
+
+import java.io._
+import scala.io.Source
+
+
+object Files {
+  def main(args: Array[String]) {
+    val filePath = "textFile.txt";
+    // Write file
+    val writer = new PrintWriter(new File(filePath));
+    writer.write("This is simple text file.");
+    writer.close();
+    // Read file
+    Source.fromFile(filePath).foreach {
+      print
+    }
+  }
+}
