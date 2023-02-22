168b952216e60a71f16fdd3c5d8fb3058966ea4f
Anatol <seddboom@gmail.com>
2023-02-22 09:18:55 +0300

    fix Collections.scala

--- src/main/scala/com/example/Collections.scala
+++ src/main/scala/com/example/Collections.scala
@@ -3,24 +3,24 @@ package com.example
 
 object IteratorDemo {
   def main(args: Array[String]) {
-    var simpleIterator = Iterator("This ", "is ", "simple ", "iterator.");
+    var simpleIterator = Iterator("This ", "is ", "simple ", "iterator.")
 
     while (simpleIterator.hasNext) {
-      print(simpleIterator.next());
+      print(simpleIterator.next())
     }
 
-    simpleIterator = Iterator("This ", "is ", "simple ", "iterator.");
+    simpleIterator = Iterator("This ", "is ", "simple ", "iterator.")
 
-    println();
-    println("Length of simpleIterator: " + simpleIterator.length);
+    println()
+    println("Length of simpleIterator: " + simpleIterator.length)
 
-    var integerIterator = Iterator(1, 2, 3, 4, 5, 6);
-    println();
-    println("Min value of integerIterator: " + integerIterator.min);
+    var integerIterator = Iterator(1, 2, 3, 4, 5, 6)
+    println()
+    println("Min value of integerIterator: " + integerIterator.min)
 
-    integerIterator = Iterator(1, 2, 3, 4, 5, 6);
+    integerIterator = Iterator(1, 2, 3, 4, 5, 6)
 
-    println("Max value of integerIterator: " + integerIterator.max);
+    println("Max value of integerIterator: " + integerIterator.max)
   }
 }
 
@@ -30,28 +30,28 @@ object IteratorDemo {
 
 object ListDemo {
   def main(args: Array[String]) {
-    val skills: List[String] = List("Java", "Scala", "SQL", "NoSQL", "Hibernate");
+    val skills: List[String] = List("Java", "Scala", "SQL", "NoSQL", "Hibernate")
 
-    val salaries: List[Int] = List(10, 20, 30, 40, 50);
+    val salaries: List[Int] = List(10, 20, 30, 40, 50)
 
-    println("skills:" + skills);
-    println("salaries: " + salaries);
+    println("skills:" + skills)
+    println("salaries: " + salaries)
   }
 }
 
 object ListConsDemo {
   def main(args: Array[String]) {
 
-    val skills = "Java" :: ("Scala" :: ("SQL" :: ("NoSQL" :: ("Hibernate" :: Nil))));
+    val skills = "Java" :: ("Scala" :: ("SQL" :: ("NoSQL" :: ("Hibernate" :: Nil))))
 
-    val salaries = 10 :: (20 :: (30 :: (40 :: (50 :: Nil))));
+    val salaries = 10 :: (20 :: (30 :: (40 :: (50 :: Nil))))
 
     val list = Nil.::(3).::(2).::(1)
     // Add to tail
     val listTail = list :+ 4
 
-    println("skills:" + skills);
-    println("salaries: " + salaries);
+    println("skills:" + skills)
+    println("salaries: " + salaries)
     println("list: " + listTail)
   }
 }
@@ -64,10 +64,10 @@ object TwoDimensionalListDemo {
       List(
         List("A", "B", "C"),
         List("D", "E", "F")
-      );
+      )
 
-    println("twoDimensionalList:");
-    println(twoDimensionalList);
+    println("twoDimensionalList:")
+    println(twoDimensionalList)
   }
 }
 
@@ -75,9 +75,9 @@ object TwoDimensionalListDemo {
 
 object NothingListDemo {
   def main(args: Array[String]) {
-    val nothingList:List[Nothing] = List();
+    val nothingList:List[Nothing] = List()
 
-    println("nothingList: " + nothingList);
+    println("nothingList: " + nothingList)
   }
 }
 
@@ -85,16 +85,16 @@ object NothingListDemo {
 
 object ListBasicOperationsDemo {
   def main(args: Array[String]) {
-    val list1 = 1 :: (2 :: (3 :: Nil));
-    val list2 = 4 :: (5 :: (6 :: Nil));
+    val list1 = 1 :: (2 :: (3 :: Nil))
+    val list2 = 4 :: (5 :: (6 :: Nil))
 
-    println("list1: " + list1);
-    println("list2: " + list2);
+    println("list1: " + list1)
+    println("list2: " + list2)
 
-    val commonList = List.concat(list1, list2);
-    println("commonList" + commonList);
+    val commonList = List.concat(list1, list2)
+    println("commonList" + commonList)
 
-    println("Reversed commonList: " + commonList.reverse);
+    println("Reversed commonList: " + commonList.reverse)
   }
 }
 
@@ -109,23 +109,23 @@ object ListBasicOperationsDemo {
 
 object MapDemo {
   def main(args: Array[String]) {
-    var users = Map(1 -> "Ivan Ivanov", 2 -> "Kolya Nikolaev", 3 -> "Viktor Viktorov");
+    var users = Map(1 -> "Ivan Ivanov", 2 -> "Kolya Nikolaev", 3 -> "Viktor Viktorov")
 
-    println("users: " + users);
-    println("users keys: " + users.keys);
-    println("users values: " + users.values);
+    println("users: " + users)
+    println("users keys: " + users.keys)
+    println("users values: " + users.values)
 
 
-    val moreUsers = Map(4 -> "Innokentii Morozov", 5 -> "Mikhail Mikheev");
+    val moreUsers = Map(4 -> "Innokentii Morozov", 5 -> "Mikhail Mikheev")
 
-    println("moreUsers: " + moreUsers);
+    println("moreUsers: " + moreUsers)
 
-    users = users ++ moreUsers;
+    users = users ++ moreUsers
 
-    println("users: " + users);
+    println("users: " + users)
 
-    println("users contains key 1: " + users.contains(1));
-    println("users contains key 100: " + users.contains(100));
+    println("users contains key 1: " + users.contains(1))
+    println("users contains key 100: " + users.contains(100))
   }
 }
 
@@ -140,18 +140,18 @@ object MapDemo {
 
 object SetBasicOperationsDemo {
   def main(args: Array[String]) {
-    val skills1 = Set("Java", "Java", "SQL", "SQL");
-    val skills2 = Set("Scala", "Scala", "Play", "Play");
+    val skills1 = Set("Java", "Java", "SQL", "SQL")
+    val skills2 = Set("Scala", "Scala", "Play", "Play")
 
-    println("skills1: " + skills1);
-    println("skills2: " + skills2);
+    println("skills1: " + skills1)
+    println("skills2: " + skills2)
 
-    val allSkills = skills1 ++ skills2;
+    val allSkills = skills1 ++ skills2
 
-    println("allSkills: " + allSkills);
+    println("allSkills: " + allSkills)
 
-    println("Max element allSkills: " + allSkills.max);
-    println("Min element allSkills: " + allSkills.min);
+    println("Max element allSkills: " + allSkills.max)
+    println("Min element allSkills: " + allSkills.min)
   }
 }
 
@@ -162,24 +162,24 @@ object SetBasicOperationsDemo {
 
 object OptionDemo {
   def main(args: Array[String]) {
-    val developers = Map("Eugene Suleimanov" -> "Java/Scala Developer", "Ivan Ivanov" -> "C++ Developer");
+    val developers = Map("Eugene Suleimanov" -> "Java/Scala Developer", "Ivan Ivanov" -> "C++ Developer")
 
-    println("developers: " + developers);
+    println("developers: " + developers)
 
-    println("developers.get(\"Eugene Suleimanov\"): " + developers.get("Eugene Suleimanov"));
-    println("developers.get(\"Ivan Ivanov\"): " + developers.get("Eugene Suleimanov"));
-    println("developers.get(\"Kolya Nikolaev\"): " + developers.get("Kolya Nikolaev"));
+    println("developers.get(\"Eugene Suleimanov\"): " + developers.get("Eugene Suleimanov"))
+    println("developers.get(\"Ivan Ivanov\"): " + developers.get("Eugene Suleimanov"))
+    println("developers.get(\"Kolya Nikolaev\"): " + developers.get("Kolya Nikolaev"))
 
 
-    println();
-    println("Using method showDevelopers...");
+    println()
+    println("Using method showDevelopers...")
 
-    println("showDevelopers(developers.get(\"Eugene Suleimanov\")): " + showDevelopers(developers.get("Eugene Suleimanov")));
-    println("showDevelopers(developers.get(\"Ivan Ivanov\")): " + showDevelopers(developers.get("Ivan Ivanov")));
-    println("showDevelopers(developers.get(\"Kolya Nikolaev\")): " + showDevelopers(developers.get("Kolya Nikolaev")));
+    println("showDevelopers(developers.get(\"Eugene Suleimanov\")): " + showDevelopers(developers.get("Eugene Suleimanov")))
+    println("showDevelopers(developers.get(\"Ivan Ivanov\")): " + showDevelopers(developers.get("Ivan Ivanov")))
+    println("showDevelopers(developers.get(\"Kolya Nikolaev\")): " + showDevelopers(developers.get("Kolya Nikolaev")))
   }
 
-  def showDevelopers(x: Option[String]) = x match {
+  def showDevelopers(x: Option[String]):String = x match {
     case Some(some)=> some;
     case None => "Developer with this name not found";
   }
@@ -192,22 +192,21 @@ object OptionDemo {
 
 object TupleDemo {
   def main(args: Array[String]) {
-    val simpleTuple = ("String", 100500, "#FFFFFF");
+    val simpleTuple = ("String", 100500, "#FFFFFF")
 
     println("simpleTuple: ")
-    simpleTuple.productIterator.foreach { element => println("Element: " + element) };
+    simpleTuple.productIterator.foreach { element => println("Element: " + element) }
 
-    val integerTuple = (1, 2, 3);
+    val integerTuple = (1, 2, 3)
 
-    val integerTupleSum = integerTuple._1 + integerTuple._2 + integerTuple._3;
+    val integerTupleSum = integerTuple._1 + integerTuple._2 + integerTuple._3
 
-    println("integerTuple: " + integerTuple);
-    println("integerTupleSum: " + integerTupleSum);
+    println("integerTuple: " + integerTuple)
+    println("integerTupleSum: " + integerTupleSum)
 
-    val stringTuple = new Tuple2("First", "Second");
+    val stringTuple = Tuple2("First", "Second")
 
-    println("stringTuple: " + stringTuple);
-    println("Swapped integerTuple: " + stringTuple.swap);
+    println("stringTuple: " + stringTuple)
+    println("Swapped integerTuple: " + stringTuple.swap)
   }
 }
-
