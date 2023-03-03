763bf0861717d9b8c8669307b3f42e3fe3f0bb3e
Anatol <seddboom@gmail.com>
2023-03-03 09:28:20 +0300

    fix InputConsole.scala

--- src/main/scala/com/example/InputConsole.scala
+++ src/main/scala/com/example/InputConsole.scala
@@ -2,8 +2,8 @@ package com.example
 
 object InputConsole {
   def main(args: Array[String]) {
-    println("Enter Your name: ");
-    val userName = scala.io.StdIn.readLine();
-    println("Nice to meet You, " + userName);
+    println("Enter Your name: ")
+    val userName = scala.io.StdIn.readLine()
+    println("Nice to meet You, " + userName)
   }
 }
