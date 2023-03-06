cb5c659bd8fa7772334f8e0cda96f70e3174ad7c
Anatol <seddboom@gmail.com>
2023-03-06 09:28:36 +0300

    fix Regex.scala

--- src/main/scala/com/example/Regex.scala
+++ src/main/scala/com/example/Regex.scala
@@ -3,14 +3,14 @@ package com.example
 
 object Regex {
   def main(args: Array[String]) {
-    val pattern = "Scala Developer".r;
-    val scalaDeveloper = "Eugene Suleimanov is a Java/Scala Developer";
+    val pattern = "Scala Developer".r
+    val scalaDeveloper = "Eugene Suleimanov is a Java/Scala Developer"
 
-    println(pattern findFirstIn scalaDeveloper);
+    println(pattern findFirstIn scalaDeveloper)
 
-    val patternC = "C".r;
-    val scalaDeveloperAlex = "Alex is C developer";
+    val patternC = "C".r
+    val scalaDeveloperAlex = "Alex is C developer"
 
-    println(patternC replaceAllIn(scalaDeveloperAlex, "Java/Scala"));
+    println(patternC replaceAllIn(scalaDeveloperAlex, "Java/Scala"))
   }
 }
