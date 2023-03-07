c3d0ced2358741807ecf7b81d81baf2407a4649d
Anatol <seddboom@gmail.com>
2023-03-07 09:08:24 +0300

    fix String.scala

--- src/main/scala/com/example/Strings.scala
+++ src/main/scala/com/example/Strings.scala
@@ -2,30 +2,30 @@ package com.example
 
 object Strings {
   // Create a string
-  val simpleStringAutoString = "This is simple String";
-  var simpleString:String = "This is simple String";
+  val simpleStringAutoString = "This is simple String"
+  var simpleString:String = "This is simple String"
 
   def main(args: Array[String]) {
-    println(simpleString);
+    println(simpleString)
 
     // String concatenation
-    val developerName: String = "Eugene";
-    val developerSpecialty: String = "Java/Scala developer";
+    val developerName: String = "Eugene"
+    val developerSpecialty: String = "Java/Scala developer"
 
-    val developerData: String = developerName + " is a " + developerSpecialty + ".";
-    println(developerData);
+    val developerData: String = developerName + " is a " + developerSpecialty + "."
+    println(developerData)
     // Interpolator S
-    val developerNameAutoString = "Eugene Suleimanov";
+    val developerNameAutoString = "Eugene Suleimanov"
 
-    println(s"Developer name: $developerNameAutoString");
-    println(s"100 * 8 = ${100 * 8}");
+    println(s"Developer name: $developerNameAutoString")
+    println(s"100 * 8 = ${100 * 8}")
 
     // Interpolator F
-    val product = "Milk";
-    val price = 12.5d;
+    val product = "Milk"
+    val price = 12.5d
 
-    println(f"$product%s price is: $price%2.2f");
+    println(f"$product%s price is: $price%2.2f")
     // Interpolator RAW
-    println(raw"This \b String ignores \n escape sequences \n");
+    println(raw"This \b String ignores \n escape sequences \n")
   }
 }
